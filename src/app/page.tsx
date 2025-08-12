8c57d21a12ab9ab244d1f630b04308336f4c2576
CROUDZ <giov.di.via@gmail.com>
2025-08-12 12:50:20 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ const Header = dynamic(() => import("@/components/Header"), { ssr: false });
 const HeroSection = dynamic(() => import("@/components/sections/HeroSection"), {
   ssr: false,
 });
-const ReviewsSection = dynamic(
+
+/*const ReviewsSection = dynamic(
   () => import("@/components/sections/ReviewsSection"),
   { ssr: false },
-);
+);*/
+
 const ModoSection = dynamic(() => import("@/components/sections/ModoSection"), {
   ssr: false,
 });
@@ -22,27 +24,15 @@ export default function Home() {
         <HeroSection />
         <div className="flex justify-center flex-col items-center after:max-w-5xl mx-auto mt-8 px-4 py-8">
           <h2 className="text-3xl md:text-4xl font-bold text-primary-dark mb-4">
-            Notre serveur
+            Bienvenue sur Osmiumcraft !
           </h2>
           <p className="text-center text-lg text-gray-700 mt-8 mx-auto max-w-5xl px-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-            culpa suscipit, totam unde molestias exercitationem laboriosam
-            quidem. Doloremque sed minima officiis. Asperiores adipisci maxime
-            ipsum aperiam eius cum sit illum. Lorem ipsum, dolor sit amet
-            consectetur adipisicing elit. Enim vel, possimus excepturi
-            reiciendis provident laudantium id praesentium quibusdam. Similique
-            corrupti ipsum cumque error libero dolore fugit tempora, fuga sint
-            nesciunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Ex, esse! Repudiandae beatae accusantium ducimus quidem ipsum
-            assumenda eaque molestiae reiciendis! Temporibus dolorem est
-            veritatis nisi deserunt ea, nemo excepturi. Nisi. Lorem ipsum, dolor
-            sit amet consectetur adipisicing elit. Rerum consectetur
-            repudiandae, tenetur aspernatur amet maxime ut voluptas debitis
-            doloribus sint neque consequuntur cumque assumenda! Dolor cupiditate
-            amet eos sequi delectus?
+            Un serveur Minecraft moddé PvP faction créé par des passionnés pour les joueurs consoles. Lancé en juillet 2024, Osmiumcraft a su rassembler une communauté soudée où fun et fair-play règnent. Après une première saison riche en aventures, la saison 2 arrive avec des constructions époustouflantes et du gameplay encore plus intense.
+            <br/>
+            Ici, pas de course à l’argent, juste du plaisir entre joueurs. Prêt à rejoindre l’aventure ?
           </p>
         </div>
-        <ReviewsSection />
+        {/*<ReviewsSection />*/}
         <ModoSection />
       </main>
       <Footer />
